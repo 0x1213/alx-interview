95a104f50c0052fade216b5636c67d600a13fcbe
ABDELHAMID EL MAAZI <abdelhamidelmaazi@gmail.com>
2024-04-25 23:55:38 +0100

    Tasks

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 def pascal_triangle(n):
-    if n <= 0: return []
-    for i in range(n):
-        print([i + 1 for i in range(i + 1)])
-
-pascal_triangle(5)
+	triangle = []
+	if n <= 0: return triangle
+	prev_row = [1]
+	triangle.append(prev_row[:])
+	for _ in range(n - 1):
+		prev_row.append(0)
+		prev_row = [0] + prev_row
+		next_row = []
+		for i in range(len(prev_row) - 1):
+			next_row.append(prev_row[i] + prev_row[i + 1])
+		triangle.append(next_row[:])
+		prev_row = next_row
+	return triangle

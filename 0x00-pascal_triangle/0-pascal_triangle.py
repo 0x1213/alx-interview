6f5feb803b7c147af4cb3ac0677b6e70b174e0f3
ABDELHAMID EL MAAZI <abdelhamidelmaazi@gmail.com>
2024-04-26 00:04:42 +0100

    Tasks

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,16 +5,19 @@
 
 
 def pascal_triangle(n):
-    """Create a function def pascal_triangle(n): that returns a list of lists
-    of integers representing the Pascal’s triangle of n
-    """
-    res = []
-    if n > 0:
-        for i in range(1, n + 1):
-            level = []
-            C = 1
-            for j in range(1, i + 1):
-                level.append(C)
-                C = C * (i - j) // j
-            res.append(level)
-    return res
+	"""Create a function def pascal_triangle(n): that returns a list of lists
+	of integers representing the Pascal’s triangle of n
+	"""
+	triangle = []
+	if n <= 0: return triangle
+	prev_row = [1]
+	triangle.append(prev_row[:])
+	for _ in range(n - 1):
+		prev_row.append(0)
+		prev_row = [0] + prev_row
+		next_row = []
+		for i in range(len(prev_row) - 1):
+			next_row.append(prev_row[i] + prev_row[i + 1])
+		triangle.append(next_row[:])
+		prev_row = next_row
+	return triangle

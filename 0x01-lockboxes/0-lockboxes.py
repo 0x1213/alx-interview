e6017bf326c565ceb72946ca307dc9d15a7ddfa6
ABDELHAMID EL MAAZI <abdelhamidelmaazi@gmail.com>
2024-05-03 01:32:02 +0100

    Tasks

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""Solves the lock boxes puzzle """
+
 
 def canUnlockAll(boxes):
+    """Determines if all the boxes can be opened"""
+    if len(boxes) == 0:
+        return False
     keys = [0]
     for key in keys:
         for box in boxes[key]:
